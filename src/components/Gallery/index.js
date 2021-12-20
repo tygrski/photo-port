c320df39c8a33b4b38a19d5cdd551113110251cb
tygrski <77369211+tygrski@users.noreply.github.com>
2021-12-20 16:44:31 -0600

    display modeal error

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -3,16 +3,16 @@ import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from "../../utils/helpers";
 // import photo from "../../assets/small/commercial/0.jpg";
 
-function Gallery(props) {
-  const { name, description } = props.currentCategory;
-  console.log(props);
+function Gallery({ currentCategory }) {
+  const { name, description } = currentCategory;
+
   
 
   return (
     <section>
       <h1 data-testid="h1tag">{capitalizeFirstLetter((name))}</h1>
       <p> {description}</p>
-      <PhotoList category={name} />
+      <PhotoList category={currentCategory.name} />
     </section>
   );
 }
