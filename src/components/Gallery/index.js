3e8f99f878a7775b3082c6f2014ff6587cf786c5
tygrski <77369211+tygrski@users.noreply.github.com>
2021-12-20 14:23:37 -0600

    display bug

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PhotoList from '../PhotoList';
-// import { capitalizeFirstLetter } from "../../utils/helpers";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 // import photo from "../../assets/small/commercial/0.jpg";
 
 function Gallery(props) {
